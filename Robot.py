5daa510b87efee8be87a1c323126c6157ca2b52e
Amar-s102 <kamar.abou.niaaj@gmail.com>
2025-04-14 11:08:15 +0300

    ooo

--- Robot.py
+++ Robot.py
@@ -1,11 +1,20 @@
+from Warehouse import *
+import heapq
+
 class Robot:
     def __init__(self, x,y):
         self.x=x
         self.y=y
         self.rotation= 'R'
         self.lift_height=0
-        self.C=0
+        self.c=0
         self.move_cost=0
+
+    def find_path(self,warehouse,target_x,target_y):
+        def heuristic(x1, y1, x2, y2):
+            return abs(x1 - x2) + abs(y1 - y2)
+        open_set=[]
+
 
     def move_forward(self, warehouse):
         if self.rotation=='N': self.y+1
@@ -66,6 +75,20 @@ class Robot:
     def deliver_box(self,warehouse):
         if not self.c: return False
         
+        if (self.x , self.y) in warehouse.delivery_points:
+            warehouse.delivered_boxes.apped(self.c)
+            self.c=None
+            return True
+        return False
+    
+    def valid_move(self,warehouse,x,y):
+        if not (0 <= x <warehouse.grid_size and 0<=y <warehouse.grid_size):
+            return False
+        
+        if warehouse.grid[x][y].shelf:
+            return False
+        
+        return True
 
     
 
